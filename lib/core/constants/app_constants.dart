ae2995dac194ad7ed9adf2b88f49f75a0089759a
karol <karolhany09@gmail.com>
2025-08-11 18:54:58 +0300

    first commit

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+const magentaColor = Color(0xff004751);
+const neonColor = Color(0xffDDE84A);
+const wightColor = Color(0xffFFFFFF);
---
+++ lib/core/helpers/assets.dart
@@ -0,0 +1,4 @@
+class AssetsData {
+  static const logo = 'assets/images/splash_icon.jpg';
+  static const onboarding = 'assets/images/onboarding.jpg';
+}
